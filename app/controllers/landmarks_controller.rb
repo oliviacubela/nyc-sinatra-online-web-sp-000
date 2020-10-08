f876bad348c81e7745d060888c1dec872a7f8f51
Olivia Cubela <oliviacubela@gmail.com>
2020-10-08 22:18:45 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,5 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
   get '/landmarks' do
     @landmarks = Landmark.all
     @figures = Figure.all
@@ -20,7 +20,6 @@ class LandmarksController < ApplicationController
     erb :"landmarks/edit"
   end
 
-  # edits landmark
   post '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.name = params['landmark']['name']

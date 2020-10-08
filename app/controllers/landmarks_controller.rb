17169602e64bee013c41d002a85b74f79cc505dc
Olivia Cubela <oliviacubela@gmail.com>
2020-10-08 21:36:27 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,4 +15,12 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/edit"
   end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save 
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 end

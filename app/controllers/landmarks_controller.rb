bd75ed8151f4cba602342aeb09d226a6138a50be
Olivia Cubela <oliviacubela@gmail.com>
2020-10-08 21:33:16 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,10 +1,10 @@
 class LandmarksController < ApplicationController
   
-  get '/landmarks' do 
-    @landmarks = Landmark.all 
-    @figures = Figure.all 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
     erb :'landmarks/index'
   end
 
-  
+
 end
